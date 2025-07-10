d3f6af46a65d4463846d1e75577ffa9820556d6f
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-10 17:00:36 +0300

    Logout button

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { useAuth } from '../context/AuthProvider'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthProvider';
 
 const Logout: React.FC = () => {
     const { Logout } = useAuth();
 
-  return (
-    <div>Logout</div>
-  )
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        Logout();
+        navigate("/login")
+    })
+
+    return (
+        <div>
+            Loggin out...
+        </div>
+    )
 }
 
 export default Logout
