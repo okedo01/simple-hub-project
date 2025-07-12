8f7b990af857a0d8b6747b02e3bfcf9d3d4e923f
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-12 12:00:25 +0300

    Register component

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../components/ui/card"
+import { Button } from '../components/ui/button';
+import { Input } from '../components/ui/input';
+import { Label } from '../components/ui/label';
 
 const Register: React.FC = () => {
-  return (
-    <div>Register</div>
-  )
-}
+    return (
+        <div className="grid justify-items-center items-center min-h-screen">
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Register for: </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div>
+                                <Label htmlFor="name">Name</Label>
+                                <Input type="text" placeholder="Enter your name"/>
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input id="email" type="email" placeholder="m@example.com" />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                </div>
+                                <Input
+                                    id="password" type="password" />
+                            </div>
+
+                            <CardFooter className="">
+                                <Button type="submit" className="w-full disabled:bg-gray-900">
+                                    Register
+                                </Button>
+                            </CardFooter>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )}
 
 export default Register
