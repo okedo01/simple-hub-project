4bc5b6893c84c9a51d949fe21a4dca7de91fe07d
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-14 15:58:03 +0300

    Theme context

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,29 +1,33 @@
 import type React from "react"
 import { Children, useContext, useState } from "react";
-import { createContext } from "vm";
+import { createContext } from "react";
 
-type themeContextType = {
-    theme: "light" | "dark";
-}
+type themeContextType = "light" | "dark";
 
 type props = {
     children: React.ReactNode;
+    toggleTheme: () => void;
 }
 
 const themeContext = createContext<themeContextType | undefined>(undefined);
 
 export const useTheme = () => {
-    const theme = useContext();
-    if(!theme) {
+    const theme = useContext(themeContext);
+    if (!theme) {
         throw new Error("useTheme must be used inside themeContext");
     }
     return theme;
 }
 
 export const ThemeProvider = ({ children }: props) => {
-    const [ mode, setMode ] => useState<theme>(dark);
-}
+    const [mode, setMode] = useState<theme>(dark);
 
-<themeContext.Provider value={{}}>
-    { children }
-</themeContext.Provider>
+    const toggleMode = () => {
+        setMode(!mode);
+    }
+    return (
+        <themeContext.Provider value={{ mode }}>
+            {children}
+        </themeContext.Provider>
+    )
+}
