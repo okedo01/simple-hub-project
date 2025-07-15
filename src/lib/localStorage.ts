a3fd7354fc389ce336f1f082c4a29198b1c9c6e1
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-15 12:07:45 +0300

    Retriving data from local storage

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,9 +1,17 @@
-//save data to local storage
-export const getItem = (key: string) => {
-    localStorage.getItem(JSON.parse(key));
-}
 
 //Retrive data from local storage
-export const setItem = (key: string, value: any) => {
-    localStorage.setItem(key, JSON.parse(value))
+export const setItem = (key: string, value: unknown): void => {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+//save data to local storage
+export const getItem = (key: string) => {
+    const item = localStorage.getItem(key);
+    if(item === null) return null;
+
+    try{
+        return JSON.parse(item);
+    } catch {
+        return null;
+    }
 }
