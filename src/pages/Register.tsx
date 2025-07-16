8ba05a5c2d97a54da032898a5b67293c0e981ed3
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-16 13:03:19 +0300

    Install hookForm Resolvers

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import type { Courses, Students } from '../lib/Types';
+import type { Courses } from '../lib/Types';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import {
     Card,
