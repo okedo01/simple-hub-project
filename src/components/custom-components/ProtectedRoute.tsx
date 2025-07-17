24019cd538adf964995da2d476e086f644f320ab
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-17 12:57:37 +0300

    Update the Protected Route page

--- src/components/custom-components/ProtectedRoute.tsx
+++ src/components/custom-components/ProtectedRoute.tsx
@@ -1,23 +1,47 @@
+// import React from 'react'
+// import { useAuth } from '../../context/AuthProvider'
+// import { Navigate } from 'react-router-dom'
+
+// type props = {
+//     children: React.ReactNode
+// }
+
+// const ProtectedRoute = ({children}: props) => {
+//     const { user } = useAuth();
+
+//     if(!user) {
+//         return <Navigate to="/login" replace />
+//     }
+
+//   return (
+//     <div>
+//         { children }
+//     </div>
+//   )
+// }
+
+// export default ProtectedRoute
+
+
+
+// src/components/custom-components/ProtectedRoute.tsx
+
 import React from 'react'
 import { useAuth } from '../../context/AuthProvider'
 import { Navigate } from 'react-router-dom'
 
-type props = {
-    children: React.ReactNode
+type Props = {
+  children: React.ReactNode
 }
 
-const ProtectedRoute = ({children}: props) => {
-    const { user } = useAuth();
+const ProtectedRoute = ({ children }: Props) => {
+  const { user } = useAuth();
 
-    if(!user) {
-        return <Navigate to="/login" replace />
-    }
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
 
-  return (
-    <div>
-        { children }
-    </div>
-  )
+  return <>{children}</>
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
