8b9ca5fdd1f186a5130161484d58602ac0c0eb40
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-28 12:38:33 +0300

    Fixing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,3 @@
-// import { Route, Routes } from "react-router-dom"
-// import Courses from "./pages/CourseLists"
-// import NotFound from "./components/custom-components/NotFound"
-// import Login from "./pages/Login"
-// import SignUp from "./pages/SignUp"
-// import ProtectedRoute from "./components/custom-components/ProtectedRoute"
-// import Logout from "./pages/Logout"
-// import Register from "./pages/Register"
-// import AdminDashboard from "./components/custom-components/AdminDashboard"
-// import Layout from "./components/custom-components/LayoutDashboard"
-// import Home from "./pages/HomePg"
-
-// function App() {
-//   return (
-//     <div className="">
-//       <Routes>
-//         {/* {Private Routes} */}
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<SignUp />} />
-
-//         {/* {Public Routes} */}
-//         <Route path="/" element={<Layout />}>
-//           <Route path="/logout" element={<Logout />} />
-//           <Route index element={<Home />} />
-//           <Route path="/courses" element={
-//             <ProtectedRoute>
-//               <Courses />
-//             </ProtectedRoute>
-//           } />
-//           <Route path="/courses/:id" element={<Register />} />
-//           <Route path="/admin" element={<AdminDashboard />} />
-//           <Route path="*" element={<NotFound />} />
-//         </Route>
-//       </Routes>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
 import { Navigate, Route, Routes } from "react-router-dom"
 import Courses from "./pages/CourseLists"
 import NotFound from "./components/custom-components/NotFound"
@@ -51,21 +9,18 @@ import Register from "./pages/Register"
 import AdminDashboard from "./components/custom-components/AdminDashboard"
 import Layout from "./components/custom-components/LayoutDashboard"
 import Home from "./pages/HomePg"
-import { useAuth } from "./context/AuthProvider" // assuming you use AuthProvider
+import { useAuth } from "./context/AuthProvider" 
 
 function App() {
-  const { user } = useAuth(); // assuming user is null if not logged in
+  const { user } = useAuth(); 
 
   return (
     <div>
       <Routes>
-        {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
 
-        {/* Public layout but protected content */}
         <Route path="/" element={<Layout />}>
-          {/* Redirect / to login if not logged in, else show Home */}
           <Route
             index
             element={
